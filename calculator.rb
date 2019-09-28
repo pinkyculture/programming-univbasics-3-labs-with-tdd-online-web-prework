50d5154ed23ce04dc934a60a4d71f62371a79262
interactive-cache-8375 <randomblue1@yahoo.com>
2019-09-28 07:15:37 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,42 @@
-# Add your variables here
+describe ".calculator.rb" do
+  it "contains a local variable called first_number that is assigned to a number" do
+first_number = 2 ('./calculator.rb', "first_number")
+
+    expect(first_number).to be_an(Integer).or be_a(Float)
+  end
+  it "contains a local variable called second_number that is assigned to a number that isn't 0" do
+    second_number = 3('./calculator.rb', "second_number")
+
+    expect(second_number).to be_an(Integer).or be_a(Float)
+    expect(second_number).not_to equal(0)
+  end
+  it "contains a local variable called sum that is assigned to the result of adding first_number and second_number" do
+    first_number = 2 ('./calculator.rb', "first_number")
+    second_number = 3 ('./calculator.rb', "second_number")
+    sum = 5 ('./calculator.rb', "sum")
+
+    expect(sum).to eq(first_number+second_number)
+  end
+it "contains a local variable called difference that is assigned to the result of subtracting first_number and second_number" do
+    first_number = 2 ('./calculator.rb', "first_number")
+    second_number = 3('./calculator.rb', "second_number")
+    difference = -1 ('./calculator.rb', "difference")
+
+    expect(difference).to eq(first_number-second_number)
+  end
+  
+  it "contains a local variable called product that is assigned to the result of multiplying first_number and second_number" do
+    first_number = 2 ('./calculator.rb', "first_number")
+    second_number = 3('./calculator.rb', "second_number")
+    product = 6('./calculator.rb', "product")
+
+    expect(product).to eq(first_number*second_number)
+  end
+  it "contains a local variable called quotient that is assigned to the result of dividing first_number by second_number" do
+    first_number = 6 ('./calculator.rb', "first_number")
+    second_number = 3 ('./calculator.rb', "second_number")
+    quotient = 2 ('./calculator.rb', "quotient")
+
+    expect(quotient).to eq(first_number/second_number)
+  end
+end
